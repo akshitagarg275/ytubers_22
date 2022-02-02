2e3e4a5dd8a0ffc7743641e86875f41239e8facf
akshita garg <akshitagarg@275@gmail.com>
2022-02-02 11:38:32 +0530

    team section

--- tubers/webpages/admin.py
+++ tubers/webpages/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Slider
+from .models import Slider,Team
+from django.utils.html import format_html
 # Register your models here.
 
+class TeamAdmin(admin.ModelAdmin):
+    def myphoto(self,object):
+        return format_html('<img width="40" src={}/>'.format(object.photo.url))
+    list_display = ('id','myphoto','fname','role','created_date')
+    list_display_links=('id','fname')
+    search_fields=('fname','role',)
+    list_filter=('role',)
+
+
+
 admin.site.register(Slider)
+admin.site.register(Team,TeamAdmin)
--- tubers/webpages/models.py
+++ tubers/webpages/models.py
@@ -13,3 +13,15 @@ class Slider(models.Model):
 
     def __str__(self):
         return self.headline
+
+class Team(models.Model):
+    fname=models.CharField(max_length=255)
+    lname=models.CharField(max_length=255)
+    role=models.CharField(max_length=255)
+    fb_link=models.CharField(max_length=255)
+    insta_link=models.CharField(max_length=255)
+    photo=models.ImageField(upload_to='media/team/%Y/%m/%d')
+    created_date=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.fname 
